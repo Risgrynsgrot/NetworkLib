de3a5848ef087e2f964e75bd0754a8e0bb1a701c
Kingminiman <kingminiman@gmail.com>
2021-04-12 20:52:49 +0200

    added changes for the portfolio

--- src/Spooder/PacketTypes.h
+++ src/Spooder/PacketTypes.h
@@ -25,31 +25,14 @@ struct TestPacket
 {
 	int myA[32];
 	float myB;
-	void Write(BitWriter& aWriter)
-	{
-		int bitsToWrite = STATIC_BITS_REQUIRED(0, 255);
-		for (size_t i = 0; i < 32; i++)
-		{
-			aWriter.WriteBits(myA + i, bitsToWrite);
-		}
-	}
-	void Read(BitReader& aReader)
-	{
-		int bitsToRead = STATIC_BITS_REQUIRED(0, 255);
-
-		for (size_t i = 0; i < 32; i++)
-		{
-			aReader.ReadBits(myA + i, bitsToRead);
-		}
-	}
 	template <typename Stream>
 	bool Serialize(Stream& aStream)
 	{
 		for (size_t i = 0; i < 32; i++)
 		{
 			SERIALIZE_INT(aStream, myA[i], 0, 255);
-			SERIALIZE_FLOAT_COMPRESSED(aStream, myB, 0.f, 256.f, 0.01f);
 		}
+		SERIALIZE_FLOAT_COMPRESSED(aStream, myB, 0.f, 256.f, 0.01f);
 		return true;
 	}
 };
